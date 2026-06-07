061616ef29d09fc2f0742e58874a439d239d0d91
Поліна Єремій <yeremiipolina@gmail.com>
2026-06-07 23:34:56 +0300

    одано FastAPI застосунок та endpoint для роботи з об'єктами

--- main.py
+++ main.py
@@ -14,3 +14,49 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = [
+    {
+        "id": 1,
+        "name": "Ноутбук",
+        "price": 35000
+    },
+    {
+        "id": 2,
+        "name": "Мишка",
+        "price": 800
+    },
+    {
+        "id": 3,
+        "name": "Клавіатура",
+        "price": 1500
+    },
+    {
+        "id": 4,
+        "name": "Монітор",
+        "price": 7000
+    }
+]
+
+
+@app.get("/items")
+def get_items():
+    return items
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+
+    for item in items:
+
+        if item["id"] == item_id:
+            return item
+
+    return {
+        "message": "Об'єкт не знайдено"
+    }
